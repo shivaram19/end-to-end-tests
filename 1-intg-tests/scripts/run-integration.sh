8d06680123eb3ebf1bc920cba09d0542962870e0
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-11-28 13:36:19 +0530

    localhost : 5432 => localhost:5431

--- 1-intg-tests/scripts/run-integration.sh
+++ 1-intg-tests/scripts/run-integration.sh
@@ -1,6 +1,6 @@
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
-./scripts/wait-for-it.sh "postgresql://postgres:password@localhost:5432/integration_test" -- 
+./scripts/wait-for-it.sh "postgresql://postgres:password@localhost:5431/integration_test?schema=public" -- 
 echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 npm run test
